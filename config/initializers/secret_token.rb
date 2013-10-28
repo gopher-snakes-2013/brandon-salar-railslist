b02373d13c4a87aa74a7973959a5afe3c2537e58
Apprentice <apprentice@dbc-21.local>
2013-10-28 15:26:40 -0700

    category index and skeleton complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigslistRails::Application.config.secret_token = 'b817e754e8ec2ef5332c0240eead3c7ef4d318d0ed28ee7142ce9e42f820e7824d2b77986e2e3c2269292170a01401e6e11dcfe745f32689dfb44d35d85d2bbd'
